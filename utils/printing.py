26d412f07094c46fda065f8d18f63e155876474d
Appleshow2020 <zzunseolee1@gmail.com>
2025-09-30 21:29:42 +0900

    Test 29w30a-2129

--- utils/printing.py
+++ utils/printing.py
@@ -16,11 +16,16 @@ class Colors:
     debug = '\033[90m'
     reset = '\033[0m'
 
-def printf(*text:object,
-           ptype:LT|None = LT.debug,
-           end:str|None = '\n',
-           sep:str|None=' ',
-           useReset:bool|None = True):
+def printf(*text:object,**kwargs):
+        #    ptype:LT|None = LT.debug,
+        #    end:str|None = '\n',
+        #    sep:str|None=' ',
+        #    useReset:bool|None = True):
+
+    ptype:LT|None = kwargs.get('ptype', LT.debug)
+    end:str|None = kwargs.get('end', '\n')  
+    sep:str|None = kwargs.get('sep', ' ')
+    useReset:bool|None = kwargs.get('useReset', True)
 
     def now2() -> str:
         return time.strftime("%X")
